e6ba5292f916bee2e83676659a3a5e71c3dbe323
Aliff Amir <aliff6406@gmail.com>
2024-12-19 18:52:45 +0000

    feat: add call to list repo commits

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,5 +1,39 @@
 import { Octokit } from "octokit";
 
 export const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
-})
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const githubUrl = "https://github.com/docker/genai-stack";
+
+type Response = {
+  commitHash: string;
+  commitMessage: string;
+  commitAuthorName: string;
+  commitAuthorAvatar: string;
+  commitDate: string;
+};
+
+export const getCommitHashes = async (
+  githubUrl: string,
+): Promise<Response[]> => {
+  const { data } = await octokit.rest.repos.listCommits({
+    owner: "docker",
+    repo: "genai-stack",
+  });
+  const sortedCommits = data.sort(
+    (a, b) =>
+      new Date(b.commit.author?.date ?? 0).getTime() -
+      new Date(a.commit.author?.date ?? 0).getTime(),
+  );
+
+  return sortedCommits.slice(0, 15).map((commit) => ({
+    commitHash: commit.sha ?? "",
+    commitMessage: commit.commit.message ?? ("" as string),
+    commitAuthorName: commit.commit.author?.name ?? "",
+    commitAuthorAvatar: commit.author?.avatar_url ?? "",
+    commitDate: commit.commit.author?.date,
+  }));
+};
+
+console.log(await getCommitHashes(githubUrl));
