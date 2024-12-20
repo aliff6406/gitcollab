5d7e1585daf5456a5ade9790d25e3ebe5cca6cdc
Aliff Amir <aliff6406@gmail.com>
2024-12-20 15:23:44 +0000

    feat: initialise gemini and add prompt to get commit diff

---
+++ src/lib/gemini.ts
@@ -0,0 +1,18 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { promptVer1 } from "./prompts";
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+export const summariseCommit = async (diff: string) => {
+  const response = await model.generateContent([
+    promptVer1,
+    `Please summarise the following file: \n\n${diff}`,
+  ]);
+
+  return response.response.text();
+};
+
+
