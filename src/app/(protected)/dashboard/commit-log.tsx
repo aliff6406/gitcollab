a9288ea0dd9b3d82ed10596b39add93b0a5adaed
Aliff Amir <aliff6406@gmail.com>
2024-12-21 17:46:05 +0000

    feat: add commit log component

--- src/app/(protected)/dashboard/commit-log.tsx
+++ src/app/(protected)/dashboard/commit-log.tsx
@@ -1,12 +1,64 @@
 "use client";
 
 import useProject from "@/hooks/use-project";
+import { cn } from "@/lib/utils";
 import { api } from "@/trpc/react";
+import { ExternalLink } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const CommitLog = () => {
-  const { projectId } = useProject();
+  const { projectId, project } = useProject();
   const { data: commits } = api.project.getCommits.useQuery({ projectId });
 
-  return <pre>{JSON.stringify(commits, null, 2)}</pre>;
+  return (
+    <>
+      <ul className="space-y-6">
+        {commits?.map((commit, idx) => {
+          return (
+            <li key={commit.id} className="relative flex gap-x-4">
+              <div
+                className={cn(
+                  idx === commits.length - 1 ? "h-5" : "-bottom-6",
+                  "absolute left-0 top-0 flex w-6 justify-center",
+                )}
+              >
+                <div className="w-px translate-x-1 bg-gray-200"></div>
+              </div>
+
+              <>
+                <img
+                  src={commit.commitAuthorAvatar}
+                  alt="Commit Author Avatar"
+                  className="relative mt-5 size-8 flex-none rounded-full bg-gray-50"
+                />
+                <div className="flex-auto rounded-md bg-white p-3 ring-1 ring-inset ring-gray-200">
+                  <div className="flex justify-between gap-x-4">
+                    <Link
+                      target="_blank"
+                      href={`${project?.githubUrl}/commits/${commit.commitHash}`}
+                      className="py-0.5 text-xs leading-5 text-gray-500"
+                    >
+                      <span className="font-medium text-gray-900">
+                        {commit.commitMessage}
+                      </span>{" "}
+                      <span className="inline-flex items-center">
+                        commited
+                        <ExternalLink className="ml-1 size-4" />
+                      </span>
+                    </Link>
+                  </div>
+                  <span className="font-semibold">{commit.commitMessage}</span>
+                  <pre className="mt-2 whitespace-pre-wrap text-sm leading-6 text-gray-500">
+                    {commit.summary}
+                  </pre>
+                </div>
+              </>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 export default CommitLog;
